dd889deb42e11b3560de0045ea9985e06e1aaabb
youssef aserrar <youssefasrar2966@gmail.com>
2023-11-08 14:59:08 +0100

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { Link, Stack } from "expo-router";
+import { Image, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+export default Home;
